68860d7944a714ca0066acd8f61fc72ebaab0c5f
Ezekeil <foseli0522a@mischools.org>
2026-09-19 13:28:18 -0500

    Project Init

---
+++ TestIOSApp/TestIOSApp/TestIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIOSAppApp.swift
+//  TestIOSApp
+//
+//  Created by Elijah Foster on 9/19/26.
+//
+
+import SwiftUI
+
+@main
+struct TestIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
